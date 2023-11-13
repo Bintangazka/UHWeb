6ac0713910d9f4c7ba9fbf69824048c0a56c2414
Bintangazka <Bintangazkapwt@gmail.com>
2023-11-13 15:09:41 +0700

    first commit

---
+++ Soal5.js
@@ -0,0 +1,10 @@
+var name = "Bintang Azka";
+var age = 17;
+var address = "Jln. Kebanggan, Sumbang";
+var hobby = "Mancing";
+buatkalimat(name, age, address, hobby);
+
+function buatkalimat(name, age, address, hobby) {
+    console.log("Nama saya " + name + ", umur saya " + age + " tahun, alamat saya di " + address + ", dan saya punya hobby yaitu " + hobby);
+}
+
